1b78be0de9496c2ecfc324f7e76e7ba39245dd25
Nageswara Rao <nageshraoj@gmail.com>
2021-09-23 14:35:27 +0530

    Inital Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { ThemeProvider } from '@material-ui/core'
+import HeaderComponent from './components/HeaderComponent'
+import CartsPage from './components/pages/CartsPage'
+import HomePage from './components/pages/HomePage'
+import ProductsPage from './components/pages/ProductsPage'
+import ProfilePage from './components/pages/ProfilePage'
+import LoginPage from './components/pages/LoginPage'
+import theme from './components/styles/theme'
+import CheckoutPage from './components/pages/CheckoutPage'
 
-function App() {
+const App = () => {
+  const isLogin = useSelector((state) => state.isLogin)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ThemeProvider theme={theme}>
+        {isLogin ? (
+          <BrowserRouter>
+            <HeaderComponent />
+            <Route path='/' exact component={HomePage} />
+            <Route path='/Home' exact component={HomePage} />
+            <Route path='/Products' exact component={ProductsPage} />
+            <Route path='/Carts' exact component={CartsPage} />
+            <Route path='/Checkout' exact component={CheckoutPage} />
+            <Route path='/Profile' exact component={ProfilePage} />
+          </BrowserRouter>
+        ) : (
+          <LoginPage />
+        )}
+      </ThemeProvider>
+    </>
+  )
 }
 
-export default App;
+export default App
